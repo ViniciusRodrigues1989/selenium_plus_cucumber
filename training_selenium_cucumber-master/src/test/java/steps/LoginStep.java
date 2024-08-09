ceb5c18af0f9b3b799c3f0a6b5a9677ea7a2ce39
ViniciusRodrigues1989 <52465206+ViniciusRodrigues1989@users.noreply.github.com>
2024-08-09 11:40:32 -0300

    Add files to new project

---
+++ training_selenium_cucumber-master/src/test/java/steps/LoginStep.java
@@ -0,0 +1,101 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.CadastroPage;
+import pages.LoginPage;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class LoginStep {
+    WebDriver driver;
+    ChromeOptions options;
+    LoginPage loginPage;
+    CadastroPage cadastroPage;
+
+    @Before
+    public void before(){
+        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/Caskroom/chromedriver/107.0.5304.62/chromedriver");
+        options = new ChromeOptions();
+        options.addArguments("--window-size=1810,1000");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        loginPage = new LoginPage(driver);
+        cadastroPage = new CadastroPage(driver);
+    }
+
+    @Dado("que eu esteja na pagina inicial:{string}")
+    public void queEstejaNaPaginaInicial(String url){
+        driver.get(url);
+    }
+
+/*    @E("possua um cadastro")
+    public void possuoCadastro(){
+        cadastroPage.clicarBotaoRegistrar();
+        cadastroPage.preencherEmail("teste@teste.com");
+        cadastroPage.preencherNome("Emerson");
+        cadastroPage.preencherSenha("123456");
+        cadastroPage.preencherConfirmacaoSenha("123456");
+        cadastroPage.selecionaSaldo();
+        cadastroPage.clicaBotaoCadastrar();
+        cadastroPage.clicaBotaoFecharContaCriada();
+    }*/
+
+    @E("possua um cadastro com os dados")
+    public void possuoCadastroDataTable(List<Map<String, String>> dataTable) throws InterruptedException {
+        String email = dataTable.get(0).get("Email");
+        String nome = dataTable.get(0).get("Nome");
+        String senha = dataTable.get(0).get("Senha");
+        String confirmacao = dataTable.get(0).get("Confirmacao");
+
+        Thread.sleep(1000);
+        cadastroPage.clicarBotaoRegistrar();
+        cadastroPage.preencherEmail(email);
+        cadastroPage.preencherNome(nome);
+        cadastroPage.preencherSenha(senha);
+        cadastroPage.preencherConfirmacaoSenha(confirmacao);
+        Thread.sleep(1000);
+        cadastroPage.selecionaSaldo();
+        cadastroPage.clicaBotaoCadastrar();
+        cadastroPage.clicaBotaoFecharContaCriada();
+    }
+
+    @Quando("eu preencher email:{string} e senha:{string}")
+    public void preencherEmailESenha(String email, String senha){
+        loginPage.preencheLoginEmail(email);
+        loginPage.preencheSenha(senha);
+    }
+
+    @E("clicar em fazer login")
+    public void clicarLogin(){
+        loginPage.clicarBotaoAcessar();
+    }
+
+    @Entao("valido que a pagina de boas vindas foi carregada")
+    public void validarPaginaBoasVindas(){
+        String url = "http://0.0.0.0:3000/home";
+        new WebDriverWait(driver, 5000).until(ExpectedConditions.urlToBe(url));
+        Assert.assertTrue(driver.getCurrentUrl().contains("/home"));
+    }
+
+    @After
+    public void after() {
+        driver.quit();
+    }
+
+
+}
